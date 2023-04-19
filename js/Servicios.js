e4b25d6d0baa3e9b34e8ede6c480669b443e57b8
Darwin Cerda <darwin.cerda@gmail.com>
2023-04-18 21:11:27 -0400

    1era entrega

--- js/Servicios.js
+++ js/Servicios.js
@@ -1,15 +1,18 @@
 /* console.log("Hola estoy aprendiendo a usar js"); */
 
-let kmRecorridos = prompt("Hola, ¿Para poder asesorarte mejor cuántos kilómetros recorriste el año pasado?");
+let kmRecorridos = prompt(
+  "Hola, ¿Para poder asesorarte mejor cuántos kilómetros recorriste el año pasado?"
+);
 
 if (kmRecorridos < 1000) {
   console.log("Es posible que una mantención al año sea suficiente.");
 } else if (kmRecorridos >= 1000 && kmRecorridos <= 5000) {
   console.log("Se recomienda realizar dos mantenciones al año.");
 } else {
-  console.log("Puede ser necesario realizar hasta tres mantenciones al año para mantener tu bicicleta en buen estado.");
+  console.log(
+    "Puede ser necesario realizar hasta tres mantenciones al año para mantener tu bicicleta en buen estado."
+  );
 }
-
 
 const SERVICIOS = {
   visita: 5000,
@@ -25,38 +28,33 @@ const SERVICIOS = {
 };
 
 let total = 0;
-let continuar = true;
 
-while (continuar) {
-  let eligeUnaOpción = "Servicios disponibles:\n";
+while (true) {
+  let serviciosDisponibles = "";
   for (const servicio in SERVICIOS) {
-    eligeUnaOpción += `- ${servicio}: ${SERVICIOS[servicio]} CLP\n`;
+    serviciosDisponibles += `- ${servicio}: ${SERVICIOS[servicio]} CLP\n`;
   }
 
-  const servicioIngresado = prompt(
-    `${eligeUnaOpción}\nIntroduce el nombre o abreviatura del servicio que necesitas (o 'out' para salir):`
-  ).toLowerCase();
-
-  if (servicioIngresado === "out") {
-    continuar = false;
-    continue;
-  }
-
-  const servicioEncontrado = Object.keys(SERVICIOS).find((servicio) =>
-    servicio.startsWith(servicioIngresado)
+  const servicioSeleccionado = prompt(
+    `Ingresa el nombre del servicio que necesitas (o 'out' para salir):
+    
+  Servicios disponibles:\n${serviciosDisponibles}\ `
   );
 
-  if (servicioEncontrado) {
-    total += SERVICIOS[servicioEncontrado];
+  if (servicioSeleccionado === "out") {
+    break;
+  }
+
+  if (SERVICIOS[servicioSeleccionado]) {
+    total += SERVICIOS[servicioSeleccionado];
     console.log(
-      `Has añadido el servicio de ${servicioEncontrado}. El total actual es de ${total}.`
+      `Has añadido el servicio de ${servicioSeleccionado}. El total actual es de ${total}.`
     );
   } else {
     alert(
-      `El servicio de ${servicioIngresado} no existe. Por favor, introduce un servicio válido.`
+      `El servicio de ${servicioSeleccionado} no existe. Por favor, introduce un servicio válido.`
     );
   }
 }
 
 console.log(`El total a pagar por los servicios solicitados es de ${total}.`);
-
